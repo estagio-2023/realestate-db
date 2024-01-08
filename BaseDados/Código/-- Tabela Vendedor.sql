3075dcc387ffd0c511785c3e7402f934c34a9f96
Pyri47 <rodrigo.barbeito@cgi.com>
2024-01-08 15:36:25 +0000

    bd

---
+++ BaseDados/Código/-- Tabela Vendedor.sql
@@ -0,0 +1,32 @@
+-- Tabela Vendedor
+CREATE TABLE vendedor (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(150),
+    telemovel VARCHAR(13),
+    email VARCHAR(150) UNIQUE,
+    CONSTRAINT vendedor_nome_not_null CHECK (nome IS NOT NULL),
+    CONSTRAINT vendedor_email_not_null CHECK (email IS NOT NULL)
+);
+
+-- Tabela Cliente
+CREATE TABLE cliente (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE,
+    palavra_passe VARCHAR(150) NOT NULL
+);
+
+-- Tabela Pedido_Visita
+CREATE TABLE pedido_visita (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(150),
+    email VARCHAR(150) NOT NULL,
+    FOREIGN KEY (fk_imovel_id) REFERENCES imovel_comodidades (fk_imovel_id)
+);
+
+-- Tabela Imovel_Comodidades
+CREATE TABLE imovel_comodidades (
+    fk_imovel_id SERIAL,
+    fk_comodidades_id SERIAL,
+    PRIMARY KEY (id_imovel, id_comodidades)
+);
